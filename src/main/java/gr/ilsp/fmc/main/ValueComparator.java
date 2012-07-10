4d36da91c874a18dec54d333bebfeb82e80d2c27
Vassilis Papavassiliou <vpapa@athenarc.gr>
2012-07-10 06:11:06 +0000

    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@1327 0bef2ffb-dd84-4658-9e0f-1853048a3206

---
+++ src/main/java/gr/ilsp/fmc/main/ValueComparator.java
@@ -0,0 +1,25 @@
+package gr.ilsp.fmc.main;
+
+import java.util.Comparator;
+import java.util.Map;
+
+//public class ValueComparator {
+
+@SuppressWarnings("rawtypes")
+public class ValueComparator implements Comparator {
+	Map base;
+	public ValueComparator(Map base) {
+		this.base = base;
+	}
+	public int compare(Object a, Object b) {
+
+		if((Integer)base.get(a) < (Integer)base.get(b)) {
+			return 1;
+		} else if((Integer)base.get(a) == (Integer)base.get(b)) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+}
+
