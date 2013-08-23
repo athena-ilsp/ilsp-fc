b8bf28d21d45c2bbf435297baa9b1564b8dd76ea
Prokopis Prokopidis <prokopis@athenarc.gr>
2013-08-23 09:41:48 +0000

    Added first version of Croatian Analyzer using the Croatian Stemmer and the stopword list provided by Nikola.
    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@1955 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fmc/utils/AnalyzerFactory.java
+++ src/main/java/gr/ilsp/fmc/utils/AnalyzerFactory.java
@@ -1,26 +1,33 @@
 package gr.ilsp.fmc.utils;
 
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.de.GermanAnalyzer;
 import org.apache.lucene.analysis.el.GreekAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.es.SpanishAnalyzer;
 import org.apache.lucene.analysis.fr.FrenchAnalyzer;
+import org.apache.lucene.analysis.hu.HungarianAnalyzer;
 import org.apache.lucene.analysis.it.ItalianAnalyzer;
 import org.apache.lucene.analysis.lv.LatvianAnalyzer;
 import org.apache.lucene.analysis.pt.PortugueseAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.lucene.util.Version;
 
-
 public class AnalyzerFactory {
+	private static final Logger LOGGER = Logger.getLogger(AnalyzerFactory.class);
 
-	String[] langs = {"de", "el", "en", "es", "fr", "it", "pt", "lv"};
+	String[] langs = {"de", "el", "en", "es", "fr", "it", "pt", "lv", "hr", "hu"};
 	List<String> langsList = Arrays.asList(langs);
 
 	public Analyzer getAnalyzer (String lang) throws Exception {
+
 		if (lang.equals("de")) {
 			return new GermanAnalyzer(Version.LUCENE_40);
 		} else	if (lang.equals("el")) {
@@ -37,9 +44,42 @@ public class AnalyzerFactory {
 			return new PortugueseAnalyzer(Version.LUCENE_40);
 		} else	if (lang.equals("lv")) {
 			return new LatvianAnalyzer(Version.LUCENE_40);
+		} else	if (lang.equals("hr") ) {
+			return new CroatianAnalyzer(Version.LUCENE_40);
+		} else	if (lang.equals("hu") ) {
+			return new HungarianAnalyzer(Version.LUCENE_40);
 		} else {
 			throw new Exception("No analyzer available for language " + lang + ".\n"
 					+ "Available languages are " + langsList.toString() + ".\n");
 		}
+		
 	}
+	
+	public static void main(String[] args) throws Exception {
+		AnalyzerFactory analyzerFactory = new AnalyzerFactory();
+		Analyzer analyzer = analyzerFactory.getAnalyzer("hr");
+		TokenStream tokenStream = analyzer.tokenStream("contents", new InputStreamReader(AnalyzerFactory.class.getClass().getResourceAsStream("/hr_stemmer_test_small.txt")));
+
+		OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
+		CharTermAttribute charTermAttribute = (CharTermAttribute) tokenStream.addAttribute(CharTermAttribute.class);
+
+		try {
+			tokenStream.reset(); // Resets this stream to the beginning. (Required)
+			while (tokenStream.incrementToken()) {
+				// Right way to get tokens
+				String stemmedToken = charTermAttribute.toString();
+
+				System.out.println("In: " + tokenStream.reflectAsString(true));
+				System.out.println("Out: " + stemmedToken);
+				
+//				System.out.println("token start offset: " + offsetAttribute.startOffset());
+//				System.out.println("token end offset: " + offsetAttribute.endOffset());
+			}
+			tokenStream.end(); // Perform end-of-stream operations, e.g. set the final offset.
+		} finally {
+			tokenStream.close(); // Release resources associated with this stream.
+			analyzer.close();
+		} 		
+	}
+	
 }
---
+++ src/test/java/gr/ilsp/fmc/utils/CroatianStemmerTest.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package gr.ilsp.fmc.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author prokopis
+ *
+ */
+public class CroatianStemmerTest {
+
+	private final static String APPNAME = CroatianStemmerTest.class.getSimpleName();
+	static Logger logger = LoggerFactory.getLogger(APPNAME);
+	
+	CroatianStemmer croatianStemmer;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		croatianStemmer = new CroatianStemmer();
+	}
+
+	/**
+	 * Test method for {@link gr.ilsp.fmc.utils.CroatianStemmer#stem()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testStem() throws Exception {
+		String inCharsetName = "UTF-8";
+		String outCharsetName = "UTF-8";
+		String inFilename = "src/test/resources/vecernji.hr.stem";
+		String outFilename = "src/test/resources/vecernji.hr.test";
+		logger.info("Reading: " + inFilename);
+		logger.info("Writing: " + outFilename);
+
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(inFilename), inCharsetName));
+			bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFilename), outCharsetName));
+			AnalyzerFactory analyzerFactory = new AnalyzerFactory();
+			Analyzer analyzer = analyzerFactory.getAnalyzer("hr");
+
+			int lines=0;
+			int hits=0;
+			
+			String line;
+			while ((line = br.readLine()) != null) {
+				lines++;
+				List<String> fields = Arrays.asList(line.split("\\s+"));
+				String token = fields.get(0).toLowerCase();
+				String stem0 = fields.get(1);
+
+				String stemmedToken = token;
+				TokenStream tokenStream = analyzer.tokenStream("contents",
+						new StringReader(token));
+				CharTermAttribute charTermAttribute = (CharTermAttribute) tokenStream
+						.addAttribute(CharTermAttribute.class);
+				try {
+					tokenStream.reset(); // Resets this stream to the beginning.
+											// (Required)
+					while (tokenStream.incrementToken()) {
+						// Right way to get tokens
+						stemmedToken = charTermAttribute.toString();
+						// System.out.println("In: " +
+						// tokenStream.reflectAsString(true));
+						// System.out.println("Out: " + stemmedToken);
+					}
+					tokenStream.end(); // Perform end-of-stream operations, e.g.
+										// set the final offset.
+				} finally {
+					tokenStream.close(); // Release resources associated with
+											// this stream.
+				}
+				if (stemmedToken.equals(stem0)) {
+					hits++;
+				} else {
+					//bw.write( fields.get(0)+ "\t"+ fields.get(1)+ "\t"+ stemmedToken +"\t********************\n");
+				}
+				bw.write( fields.get(0)+ "\t"+ stemmedToken +"\n");
+			}
+			analyzer.close();
+
+			if (br != null)
+				br.close();
+			if (bw != null)
+				bw.close();
+			logger.info("Done");
+			logger.info("Acc: "+ ((double) hits / lines));
+
+		} catch (FileNotFoundException e) {
+			logger.error(e.getMessage());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+}
