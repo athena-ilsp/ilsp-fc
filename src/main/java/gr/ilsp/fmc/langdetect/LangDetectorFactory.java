88e3a9bbbcd007c982bb3018124328eec65f258d
Prokopis Prokopidis <prokopis@athenarc.gr>
2013-08-27 07:43:55 +0000

    Ported to Java and added Nicola Ljubesi's naivebayesclassifier for bs-hr-sr language detection.
    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@1959 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fmc/langdetect/LangDetectorFactory.java
+++ src/main/java/gr/ilsp/fmc/langdetect/LangDetectorFactory.java
@@ -3,15 +3,19 @@
  */
 package gr.ilsp.fmc.langdetect;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LangDetectorFactory {
 
+
 	private static final Logger logger = LoggerFactory.getLogger(LangDetectorFactory.class);
 
 	private String[] langDetectorIds = {
-		"langdetect", 
+		"langdetect",  
+		"bs-hr-sr-nb", // naivebayes classifier only for bs-hr-sr
 		"tika",
 		"langid"};
 
@@ -23,8 +27,10 @@ public class LangDetectorFactory {
 			return new LangIdDetector();
 		} else if (langDetector.equalsIgnoreCase("tika")) {
 			return new TikaLangDetector();
+		} else if (langDetector.equalsIgnoreCase("bs-hr-sr-nb")) {	
+			return new NaiveBayesClassifier();
 		} else {
-			logger.warn("LangDetector " + langDetector + " not among known langDetectors: " + langDetectorIds);
+			logger.warn("LangDetector " + langDetector + " not among known langDetectors: " + Arrays.toString(langDetectorIds));
 			logger.warn("Using default langDetector langdetect");
 			return new CybozuLangDetector();
 		}
@@ -46,9 +52,21 @@ public class LangDetectorFactory {
 	
 	public static void main(String[] args) throws Exception {
 		LangDetectorFactory langDetectorFactory = new LangDetectorFactory();
-		LangDetector langDetector = langDetectorFactory.getLangDetector("langdetect"); // or tika or langid
+		LangDetector langDetector = langDetectorFactory.getLangDetector("bs-hr-sr-nb"); // or tika or langid or bs-hr-sr-nb or langdetect
 		langDetector.initialize();
-		logger.info(langDetector.detect("Μια πρόταση στα ελληνικά."));
+		// logger.info(langDetector.detect("Μια πρόταση στα ελληνικά."));		
+		logger.info(langDetector.detect(croatianSent));
+		logger.info(langDetector.detect(bosnianSent));
 	}
+
+	private static String croatianSent = 
+			"Dok se turski premijer Recep Tayyip Erdogan suočavao s masovnim prosvjedima u gradovima diljem zemlje, " +
+			"ljudi u nekoliko balkanskih država iskazali su svoju potporu premijeru." +
+			"";
+	private static String bosnianSent = 
+			"Dok se premijer Recep Tayyip Erdogan suočavao s masovnim protestima u gradovima širom zemlje, " +
+			"ljudi u nekoliko balkanskih zemalja izašli su da pokažu svoju podršku premijeru." +			
+			"";
+
 
 }
