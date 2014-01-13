39b0dbe05d0358212e82e7422cdbcbe2b65047bf
Vassilis Papavassiliou <vpapa@athenarc.gr>
2014-01-13 14:54:34 +0000

    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@2161 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fmc/main/ReadResources.java
+++ src/main/java/gr/ilsp/fmc/main/ReadResources.java
@@ -1,8 +1,17 @@
 package gr.ilsp.fmc.main;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -19,4 +28,34 @@ public class ReadResources {
         in.close();
         return param;
     }
+    
+    
+    public static String readFileAsString(String filePath) throws java.io.IOException{
+		byte[] buffer = new byte[(int) new File(filePath).length()];
+		BufferedInputStream f = new BufferedInputStream(new FileInputStream(filePath));
+		f.read(buffer);
+		f.close();
+		return new String(buffer);
+	}
+    
+    
+    public static void writetextfile(String filename,String text) {
+		Writer out;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename),"UTF-8"));
+			out.write(text.trim());
+			out.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			System.err.println("Error in writing the output text file. The encoding is not supported.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Error in writing the output text file. The file does not exist.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Error in writing the output text file.");
+		}
+	}
+    
+    
 }
