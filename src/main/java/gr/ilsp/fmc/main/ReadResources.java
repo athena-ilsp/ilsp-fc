f5a56450b523c8cdb7af86af58a73a8985ab4d2b
Vassilis Papavassiliou <vpapa@athenarc.gr>
2014-01-15 10:35:34 +0000

    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@2165 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fmc/main/ReadResources.java
+++ src/main/java/gr/ilsp/fmc/main/ReadResources.java
@@ -15,6 +15,16 @@ import java.io.Writer;
 import java.net.URL;
 import java.util.ArrayList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class ReadResources {
 	
     public static ArrayList<Double> readFile(String filename) throws IOException {
@@ -28,8 +38,7 @@ public class ReadResources {
         in.close();
         return param;
     }
-    
-    
+        
     public static String readFileAsString(String filePath) throws java.io.IOException{
 		byte[] buffer = new byte[(int) new File(filePath).length()];
 		BufferedInputStream f = new BufferedInputStream(new FileInputStream(filePath));
@@ -37,8 +46,7 @@ public class ReadResources {
 		f.close();
 		return new String(buffer);
 	}
-    
-    
+        
     public static void writetextfile(String filename,String text) {
 		Writer out;
 		try {
@@ -56,6 +64,105 @@ public class ReadResources {
 			System.err.println("Error in writing the output text file.");
 		}
 	}
+ 
+    public static String extractLangfromXML(String inputString, String ele_name, String attr_name) {
+		String result="";
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(inputString);
+			doc.getDocumentElement().normalize();
+			NodeList nodeLstP = doc.getElementsByTagName(ele_name);
+			for(int s=0; s<nodeLstP.getLength() ; s++){
+				Element NameElement = (Element)nodeLstP.item(s);
+				if (NameElement.hasAttribute(attr_name)){
+					result+=NameElement.getAttributeNode(attr_name).getTextContent();
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
     
+    public static String extractTextfromXML_clean(String inputString, String ele_name, String attr_name) {
+		String result="";
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(inputString);
+			doc.getDocumentElement().normalize();
+			NodeList nodeLstP = doc.getElementsByTagName(ele_name);
+			for(int s=0; s<nodeLstP.getLength() ; s++){
+				Element NameElement = (Element)nodeLstP.item(s);
+				if (!NameElement.hasAttribute(attr_name)){
+					result+=NameElement.getTextContent();
+					result+="\n";
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+    
+    public static void copy(String fromFileName, String toFileName)
+			throws IOException {
+		File fromFile = new File(fromFileName);
+		File toFile = new File(toFileName);
+
+		if (!fromFile.exists())
+			throw new IOException("FileCopy: " + "no such source file: "
+					+ fromFileName);
+		if (!fromFile.isFile())
+			throw new IOException("FileCopy: " + "can't copy directory: "
+					+ fromFileName);
+		if (!fromFile.canRead())
+			throw new IOException("FileCopy: " + "source file is unreadable: "
+					+ fromFileName);
+
+		if (toFile.isDirectory())
+			toFile = new File(toFile, fromFile.getName());
+
+		if (toFile.exists()) {
+			if (!toFile.canWrite())
+				throw new IOException("FileCopy: "
+						+ "destination file is unwriteable: " + toFileName);
+			System.out.print("Overwrite existing file " + toFile.getName()
+					+ "? (Y/N): ");
+			System.out.flush();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			String response = in.readLine();
+			if (!response.equals("Y") && !response.equals("y"))
+				throw new IOException("FileCopy: "
+						+ "existing file was not overwritten.");
+		} else {
+			String parent = toFile.getParent();
+			if (parent == null)
+				parent = System.getProperty("user.dir");
+			File dir = new File(parent);
+			if (!dir.exists())
+				throw new IOException("FileCopy: "
+						+ "destination directory doesn't exist: " + parent);
+			if (dir.isFile())
+				throw new IOException("FileCopy: "
+						+ "destination is not a directory: " + parent);
+			if (!dir.canWrite())
+				throw new IOException("FileCopy: "
+						+ "destination directory is unwriteable: " + parent);
+		}
+		FileUtils.copyFile(fromFile,toFile);
+	}
     
 }
