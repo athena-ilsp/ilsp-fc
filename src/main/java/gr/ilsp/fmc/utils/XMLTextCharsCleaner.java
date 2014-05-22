bd27115dfe06abeb65068a301b84805cabfdf573
Vassilis Papavassiliou <vpapa@athenarc.gr>
2014-05-22 12:12:33 +0000

    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@2423 0bef2ffb-dd84-4658-9e0f-1853048a3206

---
+++ src/main/java/gr/ilsp/fmc/utils/XMLTextCharsCleaner.java
@@ -0,0 +1,65 @@
+package gr.ilsp.fmc.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.tidy.Tidy;
+
+
+public class XMLTextCharsCleaner {
+
+	 private static final Logger logger = LoggerFactory.getLogger(XMLTextCharsCleaner.class);
+     Tidy tidy = null;
+
+     public static void main(String[] args) throws IOException  {
+         XMLTextCharsCleaner app = new XMLTextCharsCleaner();
+         File inputFile = new File(args[0]);
+         File outputFile = new File(args[1]);
+         logger.info("Started examining files:");
+         logger.info("File1: " + inputFile.toString());
+         logger.info("File2: " + outputFile.toString());
+         app.initialize();
+         app.xmlTextCharsClean(inputFile, outputFile);
+     }
+
+     private void initialize() {
+         tidy = new Tidy();
+
+         tidy.setInputEncoding("UTF-8");
+         tidy.setOutputEncoding("UTF-8");
+         tidy.setXmlTags(true);
+         tidy.setWraplen(Integer.MAX_VALUE);
+         tidy.setForceOutput(true) ;
+         tidy.setXmlOut(true);
+     }
+
+     private void xmlTextCharsClean(File inputFile, File outputFile) throws IOException {
+         InputStream inputStream = new FileInputStream(inputFile);
+         OutputStream outputStream = new FileOutputStream(outputFile);
+         tidy.parseDOM(inputStream, outputStream);
+         outputStream.close();
+         inputStream.close();
+     }
+
+	public static void clean(String infile, String outfile) {
+		XMLTextCharsCleaner app = new XMLTextCharsCleaner();
+        File inputFile = new File(infile);
+        File outputFile = new File(outfile);
+        logger.info("Started examining files:");
+        logger.info("File1: " + inputFile.toString());
+        logger.info("File2: " + outputFile.toString());
+        app.initialize();
+        try {
+			app.xmlTextCharsClean(inputFile, outputFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+}
