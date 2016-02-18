763e4c41c5472f59c9e17481ff565dfe5be637c1
Prokopis Prokopidis <prokopis@athenarc.gr>
2016-02-18 10:23:52 +0000

    Emptying trunk to prepare for 2.2.3-snapshot
    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@4089 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fmc/operations/StatusOutputCounters.java
+++
@@ -1,7 +0,0 @@
-package gr.ilsp.fmc.operations;
-
-public enum StatusOutputCounters {
-	STATUS_FILTERED,   // successfully classified a document.
-	STATUS_FAILED    // failed to classify a document
-}
-
