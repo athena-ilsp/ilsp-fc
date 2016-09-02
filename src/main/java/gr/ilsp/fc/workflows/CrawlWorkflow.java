19fbfe100ab1a98b412b31c34e25e3e81ee70808
Vassilis Papavassiliou <vpapa@athenarc.gr>
2016-09-02 13:08:36 +0000

    First steps on handling a list with special links' attributes which indicate that they are links pointing to "translations"
    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@4470 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fc/workflows/CrawlWorkflow.java
+++ src/main/java/gr/ilsp/fc/workflows/CrawlWorkflow.java
@@ -35,6 +35,7 @@ import java.util.Comparator;
 //import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -275,18 +276,17 @@ public class CrawlWorkflow {
 	
 	
 	public static Flow createFlow(Path curWorkingDirPath, Path crawlDbPath, UserAgent userAgent, FetcherPolicy fetcherPolicy,
-			BaseUrlFilter urlFilter, HashMap<String, String> maplangs, String[] targeted_langs, String[] classes, ArrayList<String[]> topic, 
+			BaseUrlFilter urlFilter, HashMap<String, String> maplangs, String[] targeted_langs, List<String[]> tranlistAttrs, String[] classes, ArrayList<String[]> topic, 
 			double abs_thres, double rel_thres, int min_uniq_terms,int max_depth,
 			CrawlOptions options) throws Throwable {
 
 		return createFlow071(curWorkingDirPath, crawlDbPath, userAgent, fetcherPolicy,
-				urlFilter, maplangs, targeted_langs, classes, topic, abs_thres, rel_thres, min_uniq_terms, max_depth, options);				
-
+				urlFilter, maplangs, targeted_langs, tranlistAttrs, classes, topic, abs_thres, rel_thres, min_uniq_terms, max_depth, options);				
 	}
 
 
 	public static Flow createFlow071(Path curWorkingDirPath, Path crawlDbPath, UserAgent userAgent, FetcherPolicy fetcherPolicy,
-			BaseUrlFilter urlFilter, HashMap<String, String> maplangs, String[] targeted_langs, String[] classes, ArrayList<String[]> topic, double abs_thres,
+			BaseUrlFilter urlFilter, HashMap<String, String> maplangs, String[] targeted_langs, List<String[]> tranlistAttrs, String[] classes, ArrayList<String[]> topic, double abs_thres,
 			double rel_thres, int min_uniq_terms,int max_depth, CrawlOptions options) throws Throwable {
 		int maxThreads = options.getThreads();
 		boolean debug = options.isDebug();
@@ -374,7 +374,8 @@ public class CrawlWorkflow {
 
 		//contentPipe is parsed. Metadata, content and links are extracted. Content is
 		//cleaned using Boilerpipe.
-		ExtendedParsePipe parsePipe = new ExtendedParsePipe(contentPipe, new SimpleNoLinksParser(keepBoiler, curWorkingDirPath.getParent().toString(), maplangs, targeted_langs));
+		ExtendedParsePipe parsePipe = new ExtendedParsePipe(contentPipe, new SimpleNoLinksParser(keepBoiler, curWorkingDirPath.getParent().toString(), 
+				maplangs, tranlistAttrs, targeted_langs));
 		//The results from the parser are forwarded to the classifier. The classifier
 		//will score the content of each fetched page. It will also score all links
 		//based on the score of the page they came from, the anchor text and the surrounding
