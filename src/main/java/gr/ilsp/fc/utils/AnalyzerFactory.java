49580cdf80ecd5fe16af91f06b75a7efc8ebe6fb
Vassilis Papavassiliou <vpapa@athenarc.gr>
2016-11-14 09:43:13 +0000

    added analyzers for some languages
    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@4665 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fc/utils/AnalyzerFactory.java
+++ src/main/java/gr/ilsp/fc/utils/AnalyzerFactory.java
@@ -7,6 +7,8 @@ import java.util.List;
 //import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
+
+import org.apache.lucene.analysis.ar.ArabicAnalyzer;
 import org.apache.lucene.analysis.bg.BulgarianAnalyzer;
 import org.apache.lucene.analysis.ca.CatalanAnalyzer;
 import org.apache.lucene.analysis.cjk.CJKAnalyzer;
@@ -17,11 +19,14 @@ import org.apache.lucene.analysis.el.GreekAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.es.SpanishAnalyzer;
 import org.apache.lucene.analysis.eu.BasqueAnalyzer;
+import org.apache.lucene.analysis.fa.PersianAnalyzer;
 import org.apache.lucene.analysis.fi.FinnishAnalyzer;
 import org.apache.lucene.analysis.fr.FrenchAnalyzer;
 import org.apache.lucene.analysis.ga.IrishAnalyzer;
 import org.apache.lucene.analysis.gl.GalicianAnalyzer;
+import org.apache.lucene.analysis.hi.HindiAnalyzer;
 import org.apache.lucene.analysis.hu.HungarianAnalyzer;
+import org.apache.lucene.analysis.id.IndonesianAnalyzer;
 import org.apache.lucene.analysis.it.ItalianAnalyzer;
 import org.apache.lucene.analysis.lt.LithuanianAnalyzer;
 import org.apache.lucene.analysis.lv.LatvianAnalyzer;
@@ -32,6 +37,9 @@ import org.apache.lucene.analysis.pt.PortugueseAnalyzer;
 import org.apache.lucene.analysis.ro.RomanianAnalyzer;
 import org.apache.lucene.analysis.ru.RussianAnalyzer;
 import org.apache.lucene.analysis.sv.SwedishAnalyzer;
+import org.apache.lucene.analysis.tr.TurkishAnalyzer;
+import org.apache.lucene.analysis.th.ThaiAnalyzer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,8 +47,8 @@ public class AnalyzerFactory {
 
 	private static final Logger logger = LoggerFactory.getLogger(AnalyzerFactory.class);
 	
-	static String[] langs = { "deu", "ell", "eng", "spa", "fra", "ita", "por", "lav", "lit", "hrv", "hun", "jpn","zho",
-			"gle", "fin", "grc", "pol", "ron", "nor", "nld", "glg", "ces", "dan", "euq", "bul", "rus", "swe", "slv", "cat", "est", "mlt", "slk" };
+	static String[] langs = { "deu", "ell", "eng", "spa", "fra", "ita", "por", "lav", "lit", "hrv", "srp", "hun", "jpn","zho", "vie", "tur", "ara", "hin","fas","isl",
+			"gle", "fin", "grc", "pol", "ron", "nor", "nld", "glg", "ces", "dan", "euq", "bul", "rus", "swe", "slv", "cat", "est", "mlt", "slk", "tha", "ind" };
 	List<String> langsList = Arrays.asList(langs);
 	
 	
@@ -73,17 +81,17 @@ public class AnalyzerFactory {
 			return new CroatianAnalyzer();
 		} else if (lang3.equals("hun")) {
 			return new HungarianAnalyzer();
-		} else if (lang3.equals("jpn") || lang3.equals("zho")) {
+		} else if (lang3.equals("jpn") || lang3.equals("zho") || lang3.equals("vie")) {
 			return new CJKAnalyzer();
 		} else if (lang3.equals("gle")) {
 			return new IrishAnalyzer();
-		} else if (lang3.equals("fin")) {
+		} else if (lang3.equals("fin") || lang3.equals("est")) { //FIXME FinnishAnalyzer for Finnish and  Estonian
 			return new FinnishAnalyzer();
 		} else if (lang3.equals("pol")) {
 			return new PolishAnalyzer();
 		} else if (lang3.equals("ron")) {
 			return new RomanianAnalyzer();
-		} else if (lang3.equals("nor")) {
+		} else if (lang3.equals("nor") || lang3.equals("isl")) {//FIXME Same analyzer for Norwegian and Icelandic !!!
 			return new NorwegianAnalyzer();
 		} else if (lang3.equals("nld")) {
 			return new DutchAnalyzer();
@@ -105,18 +113,32 @@ public class AnalyzerFactory {
 			return new SloveneAnalyzer();
 		} else if (lang3.equals("cat")) {
 			return new CatalanAnalyzer();
-		} else if (lang3.equals("est")) {
-			return new FinnishAnalyzer(); //FIXME FinnishAnalyzer since Estonian is not available
 		} else if (lang3.equals("mlt")) {
 			return new MalteseAnalyzer(); 
-		} else if (lang3.equals("slk-hunspell")) {
+		}else if (lang3.equals("tur")) {
+			return new TurkishAnalyzer(); 
+		}else if (lang3.equals("ara")) {
+			return new ArabicAnalyzer(); 
+		}else if (lang3.equals("hin")) {
+			return new HindiAnalyzer(); 
+		}else if (lang3.equals("fas")) {
+			return new PersianAnalyzer(); 
+		}else if (lang3.equals("tha")) {
+			return new ThaiAnalyzer(); 
+		}else if (lang3.equals("ind")) {
+			return new IndonesianAnalyzer(); 
+		}
+		
+		else if (lang3.equals("slk-hunspell")) {
 			// FIXME Obviously this if and the hunspell-analyzer will never be reached due to the CzechAnalyzer if above. 
 			// Our current hunspell-based stemmer seems to perform worse on a very small test case.
 			return new SlovakAnalyzer(); 
 		} 
 		else {
-			throw new Exception("No analyzer available for language " + lang3 + ".\n" + 
-					"Available languages are " + langsList.toString() + ".\n");
+			//throw new Exception("No analyzer available for language " + lang3 + ".\n" + 
+			//		"Available languages are " + langsList.toString() + ".\n");
+			logger.error("No analyzer available for language " + lang3 + ".\n" + 	"Available languages are " + langsList.toString() + ".\n"+ " English Analyzer is used!");
+			return new EnglishAnalyzer();
 		}
 	}
 
