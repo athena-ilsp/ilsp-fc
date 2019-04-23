4b139ec1bcf9c07b937c12a0a0d70dfc048835c5
Vassilis Papavassiliou <vpapa@athenarc.gr>
2019-04-23 09:15:12 +0000

    git-svn-id: file:///opt/subversion/svn/ilsp-can/ilsp-fc/trunk@5474 0bef2ffb-dd84-4658-9e0f-1853048a3206

--- src/main/java/gr/ilsp/fc/aligner/factory/HunalignAligner.java
+++ src/main/java/gr/ilsp/fc/aligner/factory/HunalignAligner.java
@@ -12,13 +12,10 @@ import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
